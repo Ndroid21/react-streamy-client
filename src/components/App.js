a3235685986be5a8f9f8fa0a58aa8ef9557be6ec
programmingArrow <nishant.kashyap2104@gmail.com>
2019-12-26 00:35:58 +0530

    added streams and header components, added semantic ui link to index.html

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './Header';
+import StreamList from './streams/StreamList';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamShow from './streams/StreamShow';
+
 
 export default function App() {
     return (
-        <div>
-            Hello from App
+        <div className="ui container">
+            <Router>
+                <div>
+                    <Header />
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/create" component={StreamCreate} />
+                    <Route path="/streams/edit" component={StreamEdit} />
+                    <Route path="/streams/delete" component={StreamDelete} />
+                    <Route path="/streams/show" component={StreamShow} />
+                </div>
+            </Router>
         </div>
     )
 }
